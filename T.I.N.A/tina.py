98371eeae6b829fd44302fb2b415fd47a65fb5b2
rafael <rcm2005@icloud.com>
2023-11-13 15:49:28 -0300

    atualizando os requisitos e chamando classe de fornecimento do horario no aruqivo main

--- T.I.N.A/tina.py
+++ T.I.N.A/tina.py
@@ -7,17 +7,18 @@ import sounddevice as sd
 import pyaudio
 import os
 import pyttsx3
+import core
 
 import json
-
+    
 from vosk import Model, KaldiRecognizer
 
 #sintese de fala
 
-engine = pyttsx3.init()
+engine = pyttsx3.init() #inicia o modelo de sintese de fala
 
-voices = engine.getProperty('voices')
-engine.setProperty("voice", voices[-1].id)
+voices = engine.getProperty('voices') #pega a propriedade voices do computador
+engine.setProperty("voice", voices[-1].id) #configura a voz do modelo
 
 
 
@@ -27,18 +28,20 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+#reconhecimento de fala
 
-
-model = Model('IA\Tina\T.I.N.A\modelo')  #definindo o modelo (o que vai interpretar a voz)
-
+model = Model('Tina\T.I.N.A\modelo')  #definindo o modelo (o que vai interpretar a voz)
+# \OneDrive\Estudos\2S\IA
 rec = KaldiRecognizer(model, 16000)      #definindo a frequencia de leitura
 
 p = pyaudio.PyAudio() #definindo o pyaudio
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000) 
 stream.start_stream()
 
-while True:    #loop que realiza a leitura do audio
-    data = stream.read(8000)
+ #loop que realiza a leitura do audio
+
+while True:   
+    data = stream.read(20000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -52,7 +55,10 @@ while True:    #loop que realiza a leitura do audio
 
             print(text)
             speak(text)
+
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
     
 
-# print(rec.FinalResult())
+print(rec.FinalResult())
 
